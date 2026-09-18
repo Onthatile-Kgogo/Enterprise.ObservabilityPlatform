34661174638c4f62a4ecc7329b4f7a99718eab2e
Onthatile Kgogo <onthatile.kgogo@yahoo.com>
2026-09-18 12:13:25 +0200

    feat: add observability context

---
+++ src/Enterprise.Observability.Core/Context/ObservabilityContextAccessor.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Enterprise.Observability.Core.Abstractions;
+using Enterprise.Observability.Core.Models;
+
+namespace Enterprise.Observability.AspNetCore.Context;
+
+public sealed class ObservabilityContextAccessor : IObservabilityContext
+{
+    public ObservabilityContext Current
+    {
+        get
+        {
+            var activity = Activity.Current;
+
+            return new ObservabilityContext
+            {
+                CorrelationId = activity?.GetBaggageItem("correlation.id"),
+                TraceId = activity?.TraceId.ToString(),
+                SpanId = activity?.SpanId.ToString(),
+                ServiceName = activity?.GetTagItem("service.name")?.ToString(),
+                Environment = activity?.GetTagItem("deployment.environment.name")?.ToString()
+            };
+        }
+    }
+}
